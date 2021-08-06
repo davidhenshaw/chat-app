2a30022c844dcb0b300d2876454e70b25763dc05
davidhenshaw <davidhenshaw2014@gmail.com>
2021-08-06 00:08:49 -0500

    Install packages and basic client

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import { useEffect } from 'react';
 
-function App() {
+const ENDPOINT = "http://172.25.60.27:4000";
+
+function App() 
+{
+
+  useEffect( () => {
+    const socket = io(ENDPOINT);
+
+    socket.on("message", message => {
+      console.log(message);
+    })
+
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello socket world!
     </div>
   );
 }
